a23017d9b13919e17fefc7d3874559f47a931a73
Alexander Karagulamos <karagulamos@gmail.com>
2022-03-28 19:45:57 +0100

    updated scheduled function example

--- src/hello-cronny/main.go
+++ src/hello-cronny/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	currentExecutionTime := time.Now().Format(time.RFC1123)
+type requestBody struct {
+	NextRun time.Time `json:"next_run"`
+}
 
-	fmt.Printf("Function executed at %s\n", currentExecutionTime)
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	requestBody := requestBody{}
+
+	if err := json.Unmarshal([]byte(request.Body), &requestBody); err != nil {
+		log.Fatalf("scheduled function error: %s", err.Error())
+	}
+
+	log.Printf("Next run at %s\n", requestBody.NextRun.Local())
 
 	return &events.APIGatewayProxyResponse{
-		StatusCode:      200,
-		Headers:         map[string]string{"Content-Type": "text/plain"},
+		StatusCode: http.StatusNoContent,
 	}, nil
 }
 
