cff9f5b0039a8e00832dfe53f3a0512baf6ab946
Karagulamos <karagulamos@gmail.com>
2022-08-18 18:13:38 +0100

    Update main.go

--- netlify/functions/hourly-schedule/main.go
+++ netlify/functions/hourly-schedule/main.go
@@ -25,7 +25,7 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 	}
 
 	log.Printf("Next run at %s\n", requestBody.NextRun.Local())
-	log.Printf("VERSION: %d", 2)
+	log.Printf("VERSION: %d", 3)
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: http.StatusNoContent,
